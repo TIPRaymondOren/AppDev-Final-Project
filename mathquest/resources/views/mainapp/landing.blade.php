93a328cf9b07161864716e591fc556629f8dc1a5
TIPRaymondOren <mrjoren@tip.edu.ph>
2024-05-07 09:22:15 +0800

    Updated UI of Landing Page

--- mathquest/resources/views/mainapp/landing.blade.php
+++ mathquest/resources/views/mainapp/landing.blade.php
@@ -12,8 +12,54 @@
 
         <!-- Styles -->
         <link href="{{ asset('css/tailwind.css') }}" rel="stylesheet" type="text/css">
+        <link href="{{ asset('css/land.css')}}" rel="stylesheet" type="text/css">
+        
+        <!-- Bootstrap CDNs -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
     </head>
-    <body class="font-sans antialiased dark:bg-black dark:text-white/50">
-        HELLO PO, THIS IS LANDING PAGE
+    <body class="font-sans antialiased">
+        <div class="main-container">
+            <div class="title">Math Quest</div>
+            <div class="btn btn-success" id="start-btn">
+                <a href="#">Start your quest now</a>
+            </div>
+            <div class="description">
+                <div class="desc-cont learn-container">
+                    <div class="title-cont learn-title">
+                        <div class="icon-container">
+                            <img src="{{ asset('images/light-bulb.png') }}" alt="" srcset="" class="icon">
+                        </div>
+                        Learn.
+                    </div>
+                    <div class="learn-desc">
+                        Access modules where you can learn step-by-step
+                    </div>
+                </div>
+                <div class="desc-cont practice-container">
+                    <div class="title-cont practice-title">
+                        <div class="icon-container">
+                            <img src="{{ asset('images/clipboard.png') }}" alt="" srcset="" class="icon">
+                        </div>
+                        Practice.
+                    </div>
+                    <div class="practice-desc">
+                        Answer assessment quizzes for you to practice and test yourself!
+                    </div>
+                </div>
+                <div class="desc-cont grow-container">
+                    <div class="title-cont grow-title">
+                        <div class="icon-container">
+                            <img src="{{ asset('images/automation.png') }}" alt="" srcset="" class="icon">
+                        </div>
+                        Grow.
+                    </div>
+                    <div class="grow-desc">
+                        Test your growth by answering different levels of quizzes and accessing leaderboards.
+                    </div>
+                </div>
+            </div>
+        </div>
     </body>
 </html>
